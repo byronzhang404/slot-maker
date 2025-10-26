15b801eb45bc94a44dd51fe9fd7411f320735962
byronzhang <zhangxuejun.1024@bytedance.com>
2025-10-26 22:19:13 +0800

    update

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -49,7 +49,7 @@ export const initAdsterra = () => {
 
   const script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = '//pl27929030.effectivegatecpm.com/96/af/c5/96afc559a4326fc3bbbb3da06ef15af6.js';
-  script.async = true;
+  script.src = '//devolutionsingclone.com/96/af/c5/96afc559a4326fc3bbbb3da06ef15af6.js';
+  //script.async = true;
   document.head.appendChild(script);
 };
